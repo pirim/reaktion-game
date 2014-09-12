eb13a8a0fe74e5d58e5eecd5f4a926871fc4b8ba
Merdin Pirildak <piri_m@Merdins-Air.fritz.box>
2014-09-12 11:52:43 +0200

    Initial Commit

---
+++ reaktion-game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reaktion-game
+//
+//  Created by Merdin Pirildak on 12.09.14.
+//  Copyright (c) 2014 Merdin Pirildak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ reaktion-game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  reaktion-game
+//
+//  Created by Merdin Pirildak on 12.09.14.
+//  Copyright (c) 2014 Merdin Pirildak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
